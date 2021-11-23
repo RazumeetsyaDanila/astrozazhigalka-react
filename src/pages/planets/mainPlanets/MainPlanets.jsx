7a4f3f234a2424c519c333c4197294cc6872e15e
RazumeetsyaDanila <danil.kozhemyakin.99@mail.ru>
2021-11-23 19:48:16 +0300

    ...

--- src/pages/planets/mainPlanets/MainPlanets.jsx
+++ src/pages/planets/mainPlanets/MainPlanets.jsx
@@ -9,6 +9,10 @@ import VenusTest from "../venus/VenusTest";
 import MercuryTest from "../mercury/MercuryTest";
 import MarsTest from "../mars/MarsTest";
 import JupiterTest from "../jupiter/JupiterTest";
+import closeImg from "../../../img/close.png";
+import instaImg from "../../../img/instagram(min).png";
+import Modal from "../../../components/UI/modal/Modal";
+import $ from "jquery";
 
 const MainPlanets = () => {
 
@@ -18,7 +22,7 @@ const MainPlanets = () => {
 
     // let answersArr = new Array(7).fill(2).map(el => new Array(20).fill(2));
 
-    let sumAnswers = [0,0,0,0,0,0,0]
+    let sumAnswers = [0, 0, 0, 0, 0, 0, 0]
     const [moonTestVisible, setMoonTestVisible] = useState(false)
     const [sunTestVisible, setSunTestVisible] = useState(false)
     const [saturnTestVisible, setSaturnTestVisible] = useState(false)
@@ -35,57 +39,61 @@ const MainPlanets = () => {
     const [marsState, setMarsState] = useState(new Array(20).fill(2))
     const [jupiterState, setJupiterState] = useState(new Array(20).fill(2))
 
+    const [modal, setModal] = useState(false)
+    const [copyBtnText, setCopyBtnText] = useState('Копировать результат')
 
+    const result = () => {
+        setCopyBtnText('Копировать результат')
+        setModal(true)
+    }
 
-    const sumResult = () => {
-        for (let i = 0; i < 20; i++){
-            sumAnswers[0] += moonState[i]
-            sumAnswers[1] += sunState[i]
-            sumAnswers[2] += saturnState[i]
-            sumAnswers[3] += venusState[i]
-            sumAnswers[4] += mercuryState[i]
-            sumAnswers[5] += marsState[i]
-            sumAnswers[6] += jupiterState[i]
-        }
-        console.log(moonState)
-        console.log(sunState)
-        console.log(saturnState)
-        console.log(venusState)
-        console.log(mercuryState)
-        console.log(marsState)
-        console.log(jupiterState)
-        console.log(sumAnswers)
+    const sum = (arr) => {
+        let output = 0
+        for (let i = 0; i < arr.length; i++) output += arr[i]
+        return output
     }
 
     const setMoonAnswers = (index, value) => {
-        setMoonState([...moonState.slice(0, index), value, ...moonState.slice(index+1, 20)])
+        setMoonState([...moonState.slice(0, index), value, ...moonState.slice(index + 1, 20)])
     }
 
     const setSunAnswers = (index, value) => {
-        setSunState([...sunState.slice(0, index), value, ...sunState.slice(index+1, 20)])
+        setSunState([...sunState.slice(0, index), value, ...sunState.slice(index + 1, 20)])
     }
 
     const setSaturnAnswers = (index, value) => {
-        setSaturnState([...saturnState.slice(0, index), value, ...saturnState.slice(index+1, 20)])
+        setSaturnState([...saturnState.slice(0, index), value, ...saturnState.slice(index + 1, 20)])
     }
 
     const setVenusAnswers = (index, value) => {
-        setVenusState([...venusState.slice(0, index), value, ...venusState.slice(index+1, 20)])
+        setVenusState([...venusState.slice(0, index), value, ...venusState.slice(index + 1, 20)])
     }
 
     const setMercuryAnswers = (index, value) => {
-        setMercuryState([...mercuryState.slice(0, index), value, ...mercuryState.slice(index+1, 20)])
+        setMercuryState([...mercuryState.slice(0, index), value, ...mercuryState.slice(index + 1, 20)])
     }
 
     const setMarsAnswers = (index, value) => {
-        setMarsState([...marsState.slice(0, index), value, ...marsState.slice(index+1, 20)])
+        setMarsState([...marsState.slice(0, index), value, ...marsState.slice(index + 1, 20)])
     }
 
     const setJupiterAnswers = (index, value) => {
-        setJupiterState([...jupiterState.slice(0, index), value, ...jupiterState.slice(index+1, 20)])
+        setJupiterState([...jupiterState.slice(0, index), value, ...jupiterState.slice(index + 1, 20)])
     }
 
 
+    const copy_in_buffer = (el) => {
+        let $tmp = $("<textarea>");
+        $("body").append($tmp);
+        $tmp.val($(el).text()).select();
+        document.execCommand("copy");
+        $tmp.remove();
+    }
+
+    const copy = () => {
+        copy_in_buffer('#result')
+        setCopyBtnText('Результат скопирован!')
+    }
 
     return (
         <div className={classes.container}>
@@ -101,11 +109,60 @@ const MainPlanets = () => {
             <SunTest questions={question.slice(20, 40)} visible={sunTestVisible} setAnswers={setSunAnswers}/>
             <SaturnTest questions={question.slice(40, 60)} visible={saturnTestVisible} setAnswers={setSaturnAnswers}/>
             <VenusTest questions={question.slice(60, 80)} visible={venusTestVisible} setAnswers={setVenusAnswers}/>
-            <MercuryTest questions={question.slice(80, 100)} visible={mercuryTestVisible} setAnswers={setMercuryAnswers}/>
+            <MercuryTest questions={question.slice(80, 100)} visible={mercuryTestVisible}
+                         setAnswers={setMercuryAnswers}/>
             <MarsTest questions={question.slice(100, 120)} visible={marsTestVisible} setAnswers={setMarsAnswers}/>
-            <JupiterTest questions={question.slice(120, 140)} visible={jupiterTestVisible} setAnswers={setJupiterAnswers}/>
+            <JupiterTest questions={question.slice(120, 140)} visible={jupiterTestVisible}
+                         setAnswers={setJupiterAnswers}/>
 
-            <Button text={'Посчитать результат'} func={sumResult} style={{marginTop: 10}}/>
+            <Button text={'Посчитать результат'} func={result} style={{marginTop: 10}}/>
+
+            <Modal visible={modal} setVisible={setModal}>
+                <div>
+                    <div className={classes.closeBtn}>
+                        <img src={closeImg} alt="" onClick={() => {
+                            setModal(false)
+                        }}/>
+                    </div>
+
+                    <div className={classes.copyArea}>
+
+                        <div id={'result'}>
+                            <p style={{
+                                textAlign: 'center',
+                                fontFamily: "'Neucha', cursive",
+                                fontSize: 28,
+                                marginBottom: 4
+                            }}>Результат: {'\n'}</p>
+
+                            <div style={{
+                                textAlign: 'center',
+                                fontFamily: "'Scada', sans-serif",
+                                fontSize: 20,
+                                lineHeight: 1.7
+                            }}>
+                                Луна: {sum(moonState)}<br/> {'\n'}
+                                Солнце: {sum(sunState)}<br/> {'\n'}
+                                Сатурн: {sum(saturnState)}<br/> {'\n'}
+                                Венера: {sum(venusState)}<br/> {'\n'}
+                                Меркурий: {sum(mercuryState)}<br/> {'\n'}
+                                Марс: {sum(marsState)}<br/> {'\n'}
+                                Юпитер: {sum(jupiterState)}<br/>
+                            </div>
+
+                        </div>
+                        <Button func={copy} text={copyBtnText}/>
+                    </div>
+
+
+                    <div className={classes.insta}>
+                        <a href="https://www.instagram.com/mat_rizg/">
+                            <img src={instaImg} alt="Перейти в инстаграм"/>
+                        </a>
+                        <p>Перейти в инстаграм, чтобы отправить результат</p>
+                    </div>
+                </div>
+            </Modal>
         </div>
     );
 };
